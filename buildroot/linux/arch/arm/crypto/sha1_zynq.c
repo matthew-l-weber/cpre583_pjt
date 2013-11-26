8423dff5bfdff74dc1b8444e5a8e04f52a5a0221
mlweber <mlweber@localhost>
2013-11-26 15:08:32 -0600

    functional test code against checked in bit/bin image in xps impl folder

--- buildroot/linux/arch/arm/crypto/sha1_zynq.c
+++ buildroot/linux/arch/arm/crypto/sha1_zynq.c
@@ -12,6 +12,7 @@
  * Copyright (c) Andrew McDonald <andrew@mcdonald.org.uk>
  * Copyright (c) Jean-Francois Dive <jef@linuxbe.org>
  * Copyright (c) Mathias Krause <minipli@googlemail.com>
+ * Copyright (c) Matthew L Weber <mlweber@iastate.edu>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -29,40 +30,45 @@
 #include <asm/byteorder.h>
 
 
-#define ZYNQ_SHA1_BASE          0x7xxxxxxx
-#define ZYNQ_SHA1_RST		(ZYNQ_SHA1_BASE + 0x00)
-#define ZYNQ_SHA1_STATUS	(ZYNQ_SHA1_BASE + 0X01)
-#define ZYNQ_SHA1_RSV		(ZYNQ_SHA1_BASE + 0x02)
-#define ZYNQ_SHA1_FINISHED	(ZYNQ_SHA1_BASE + 0x03)
-#define ZYNQ_SHA1_H0		(ZYNQ_SHA1_BASE + 0x04)
-#define ZYNQ_SHA1_HASH_BASE	(ZYNQ_SHA1_BASE + 0x04)
-#define ZYNQ_SHA1_HASH_SIZE	(ZYNQ_SHA1_BASE + 0x05)
-#define ZYNQ_SHA1_H1		(ZYNQ_SHA1_BASE + 0x05)
-#define ZYNQ_SHA1_H2		(ZYNQ_SHA1_BASE + 0x05)
-#define ZYNQ_SHA1_H3		(ZYNQ_SHA1_BASE + 0x06)
-#define ZYNQ_SHA1_H4		(ZYNQ_SHA1_BASE + 0x07)
-#define ZYNQ_SHA1_DATA_BASE	(ZYNQ_SHA1_BASE + 0x08)
-#define ZYNQ_SHA1_DATA_NUM_REGS	(ZYNQ_SHA1_BASE + 0x20)
-#define ZYNQ_SHA1_D0		(ZYNQ_SHA1_BASE + 0x08)
-#define ZYNQ_SHA1_D1		(ZYNQ_SHA1_BASE + 0x09)
-#define ZYNQ_SHA1_D2		(ZYNQ_SHA1_BASE + 0x0A)
-#define ZYNQ_SHA1_D3		(ZYNQ_SHA1_BASE + 0x0B)
-#define ZYNQ_SHA1_D4		(ZYNQ_SHA1_BASE + 0x0C)
-#define ZYNQ_SHA1_D5		(ZYNQ_SHA1_BASE + 0x0D)
-#define ZYNQ_SHA1_D6		(ZYNQ_SHA1_BASE + 0x0E)
-#define ZYNQ_SHA1_D7		(ZYNQ_SHA1_BASE + 0x0F)
-#define ZYNQ_SHA1_D8		(ZYNQ_SHA1_BASE + 0x10)
-#define ZYNQ_SHA1_D9		(ZYNQ_SHA1_BASE + 0x11)
-#define ZYNQ_SHA1_D10		(ZYNQ_SHA1_BASE + 0x12)
-#define ZYNQ_SHA1_D11		(ZYNQ_SHA1_BASE + 0x13)
-#define ZYNQ_SHA1_D12		(ZYNQ_SHA1_BASE + 0x14)
-#define ZYNQ_SHA1_D13		(ZYNQ_SHA1_BASE + 0x15)
-#define ZYNQ_SHA1_D14		(ZYNQ_SHA1_BASE + 0x16)
-#define ZYNQ_SHA1_D15		(ZYNQ_SHA1_BASE + 0x17)
-#define ZYNQ_SHA1_FSM		(ZYNQ_SHA1_BASE + 0x1E)
+static void *memPtr;
 
-#define IO_SHA_READ(adr)         *(u32*)adr
-#define IO_SHA_WRITE(adr,val)    *(u32*)adr = (u32)val
+#define ZYNQ_SHA1_BASE		0x7AA00000
+#define ZYNQ_SHA1_REGION_SIZE	0x00000080
+
+#define ZYNQ_SHA1_RST		0x00
+#define ZYNQ_SHA1_STATUS	0x04
+#define ZYNQ_SHA1_STARTBLK	0x08
+#define ZYNQ_SHA1_FINISHED	0x0C
+#define ZYNQ_SHA1_H0		0x10
+#define ZYNQ_SHA1_HASH_BASE	0x10
+#define ZYNQ_SHA1_HASH_SIZE	0x14
+#define ZYNQ_SHA1_H1		0x14
+#define ZYNQ_SHA1_H2		0x18
+#define ZYNQ_SHA1_H3		0x1C
+#define ZYNQ_SHA1_H4		0x20
+#define ZYNQ_SHA1_DATA_BASE	0x24
+#define ZYNQ_SHA1_DATA_NUM_REGS	0x20
+#define ZYNQ_SHA1_D0		0x24
+#define ZYNQ_SHA1_D1		0x28
+#define ZYNQ_SHA1_D2		0x2C
+#define ZYNQ_SHA1_D3		0x30
+#define ZYNQ_SHA1_D4		0x34
+#define ZYNQ_SHA1_D5		0x38
+#define ZYNQ_SHA1_D6		0x3C
+#define ZYNQ_SHA1_D7		0x40
+#define ZYNQ_SHA1_D8		0x44
+#define ZYNQ_SHA1_D9		0x48
+#define ZYNQ_SHA1_D10		0x4C
+#define ZYNQ_SHA1_D11		0x50
+#define ZYNQ_SHA1_D12		0x54
+#define ZYNQ_SHA1_D13		0x58
+#define ZYNQ_SHA1_D14		0x5C
+#define ZYNQ_SHA1_D15		0x60
+// .....
+#define ZYNQ_SHA1_FSM		0x78
+
+#define IO_SHA_READ(offset)         ioread32(memPtr + offset)
+#define IO_SHA_WRITE(offset, val)   iowrite32((u32)val, memPtr + offset)
 
 struct SHA1_CTX {
 	uint32_t h0,h1,h2,h3,h4;
@@ -98,35 +104,71 @@ static void printBlk(const u8* buf, unsigned int size)
 	printk("]\n");
 }
 
+static void printAlgoState(void)
+{
+	char * stateStr;
+	unsigned int currentState = IO_SHA_READ(ZYNQ_SHA1_FSM);
+	switch(currentState)
+	{
+		case 0x0:
+			stateStr = "RST";
+			break;
+		case 0x1:
+			stateStr = "WAIT";
+			break;
+		case 0x2:
+			stateStr = "DXFER";
+			break;
+		case 0x3:
+			stateStr = "HWAIT";
+			break;
+		case 0x4:
+			stateStr = "DONE";
+			break;
+		default:
+			stateStr = "UNKWN";
+	}
+	printk("SHA1 FSM[%s]\n", stateStr);
+}
 
 static int __sha1_update(struct SHA1_CTX *sctx, const u8 *data,
 			       unsigned int len, unsigned int partial)
 {
 	unsigned int done = 0, i = 0;
+	unsigned int rounds = 0;
 
 	sctx->count += len;
 	if (partial) {
-//		printk("__sha1_update --> len:%u.(0x%x) partial:%u.(0x%x) cnt:%llu.(0x%x)\n",len, len, partial, partial, sctx->count,(u32) sctx->count);
+		printk("__sha1_update --> len:%u.(0x%x) partial:%u.(0x%x) cnt:%llu.(0x%x)\n",
+			len, len, partial, partial, sctx->count,(u32) sctx->count);
 		done = SHA1_BLOCK_SIZE - partial;
 		memcpy(sctx->data + partial, data, done);
 		printBlk(sctx->data,SHA1_BLOCK_SIZE);
 //		sha1_block_data_order(sctx, sctx->data, 1);
-		(void)memcpy((void*)ZYNQ_SHA1_DATA_BASE,
-			(void*)(sctx->data),
+		(void)memcpy_toio(
+			(u32*)(memPtr+ZYNQ_SHA1_DATA_BASE),
+			(u32*)(sctx->data),
 			SHA1_BLOCK_SIZE);
+		IO_SHA_WRITE(ZYNQ_SHA1_STARTBLK, 0x1);
+		IO_SHA_WRITE(ZYNQ_SHA1_STARTBLK, 0x0);  //hack until vhdl is fixed
 	}
 
 	if (len - done >= SHA1_BLOCK_SIZE) {
-//		printk("__sha1_update --> len:%u.(0x%x) done:%u.(0x%x) cnt:%llu.(0x%x)\n",len, len, done, done, sctx->count,(u32) sctx->count);
-		const unsigned int rounds = (len - done) / SHA1_BLOCK_SIZE;
+		printk("__sha1_update --> len:%u.(0x%x) done:%u.(0x%x) cnt:%llu.(0x%x)\n",
+			len, len, done, done, sctx->count,(u32) sctx->count);
+		rounds = (len - done) / SHA1_BLOCK_SIZE;
 //		sha1_block_data_order(sctx, data + done, rounds);
 		for(i = 0; i < rounds; i++)
 		{
-			//!!!!!!!!!!!!!!!!!!!  MUST check status here to make sure it's ready for more.....
-			while(IO_SHA_READ(ZYNQ_SHA1_STATUS) != 0x1); // Wait for Rdy for data
-			(void)memcpy((void*)ZYNQ_SHA1_DATA_BASE,
-				(void*)((data + done)+(i*SHA1_BLOCK_SIZE)),
+			// MUST check status here to make sure it's ready for more.....
+			while(IO_SHA_READ(ZYNQ_SHA1_STATUS) != 0x1);
+			(void)memcpy_toio(
+				(u32*)(memPtr+ZYNQ_SHA1_DATA_BASE),
+				(u32*)((data + done)+(i*SHA1_BLOCK_SIZE)),
 				SHA1_BLOCK_SIZE);
+			//tell algo to start, eventually this could be trigger in fabric on the last word to write.
+			IO_SHA_WRITE(ZYNQ_SHA1_STARTBLK, 0x1);  
+			IO_SHA_WRITE(ZYNQ_SHA1_STARTBLK, 0x0);  //hack until vhdl is fixed
 		}
 		printBlk(data+done,rounds * SHA1_BLOCK_SIZE);
 		done += rounds * SHA1_BLOCK_SIZE;
@@ -142,13 +184,13 @@ static int sha1_update(struct shash_desc *desc, const u8 *data,
 	struct SHA1_CTX *sctx = shash_desc_ctx(desc);
 	unsigned int partial = sctx->count % SHA1_BLOCK_SIZE;
 	int res;
-
+printAlgoState();
 	if(!sctx->count)  // reset the status bit and signal new msg
 	{
-//		IO_SHA_WRITE(ZYNQ_SHA1_FINISHED, 0x0);
-		IO_SHA_WRITE(ZYNQ_SHA1_RST, 0x1);
+		IO_SHA_WRITE(ZYNQ_SHA1_RST, 0x1); 
+		IO_SHA_WRITE(ZYNQ_SHA1_RST, 0x0);  //hack until vhdl is fixed
 	}
-
+printAlgoState();
 	/* Handle the fast case right here */
 	if (partial + len < SHA1_BLOCK_SIZE) {
 		sctx->count += len;
@@ -179,7 +221,7 @@ static int sha1_final(struct shash_desc *desc, u8 *out)
 	index = sctx->count % SHA1_BLOCK_SIZE;
 	padlen = (index < 56) ? (56 - index) : ((SHA1_BLOCK_SIZE+56) - index);
 	/* We need to fill a whole block for __sha1_update() */
-//printk("__sha1_final --> padlen:%u. index:%u.\n",padlen,index);
+	printk("__sha1_final --> padlen:%u. index:%u.\n",padlen,index);
 	if (padlen <= 56) {
 		sctx->count += padlen;
 		memcpy(sctx->data + index, padding, padlen);
@@ -188,13 +230,18 @@ static int sha1_final(struct shash_desc *desc, u8 *out)
 	}
 	__sha1_update(sctx, (const u8 *)&bits, sizeof(bits), 56);
 
-
-	while(IO_SHA_READ(ZYNQ_SHA1_STATUS) != 0x1); // Wait for Rdy for data before requesting hash
-
+	// Wait for Rdy for data before requesting hash
+	while(IO_SHA_READ(ZYNQ_SHA1_STATUS) != 0x1); 
+printAlgoState();
+	// Request hash
 	IO_SHA_WRITE(ZYNQ_SHA1_FINISHED, 0x1);
-
+	IO_SHA_WRITE(ZYNQ_SHA1_FINISHED, 0x0); //hack until vhdl is fixed
+printAlgoState();
 	while(IO_SHA_READ(ZYNQ_SHA1_STATUS) != 0x2); // Wait for HASH to be rdy
-	(void)memcpy((u32 *)sctx, (void*)ZYNQ_SHA1_HASH_BASE, ZYNQ_SHA1_HASH_SIZE);
+	(void)memcpy_fromio(
+		(u32 *)sctx, 
+		(u32*)(memPtr+ZYNQ_SHA1_HASH_BASE), 
+		ZYNQ_SHA1_HASH_SIZE);
 	/* Store state in digest */
 	printk("HASH: ");
 	for (i = 0; i < 5; i++)
@@ -247,6 +294,22 @@ static struct shash_alg alg = {
 
 static int __init sha1_mod_init(void)
 {
+	if (!request_mem_region(ZYNQ_SHA1_BASE, ZYNQ_SHA1_REGION_SIZE, "zynq_sha1")) 
+	{
+		printk("request_mem_region failed\n");
+		return -ENXIO;
+	}
+
+	memPtr = ioremap(ZYNQ_SHA1_BASE, ZYNQ_SHA1_REGION_SIZE);
+	if(memPtr == NULL)
+	{
+		printk("I/O remap failed\n");
+		release_mem_region(ZYNQ_SHA1_BASE, ZYNQ_SHA1_REGION_SIZE);
+		return -ENOMEM;
+	}
+	printk("Mapped 0x%x \n",(u32)memPtr);
+	printAlgoState();
+
 	return crypto_register_shash(&alg);
 }
 
@@ -254,6 +317,8 @@ static int __init sha1_mod_init(void)
 static void __exit sha1_mod_fini(void)
 {
 	crypto_unregister_shash(&alg);
+	iounmap(memPtr);
+	release_mem_region(ZYNQ_SHA1_BASE, ZYNQ_SHA1_REGION_SIZE);
 }
 
 
